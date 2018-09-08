8f3b0d5dc741271fff76e30e69939d8ba1d9a568
Tara Craig <tara.craig7@gmail.com>
2018-09-07 22:08:54 -0600

    Add validation test for uniqueness for station name and slug. Add uniqueness to station model for name and slug. Add slug attribute to test data for station index spec.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -6,5 +6,8 @@ describe Station, type: :model do
     it { should validate_presence_of(:dock_count) }
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:installation_date) }
+
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:slug) }
   end
 end
