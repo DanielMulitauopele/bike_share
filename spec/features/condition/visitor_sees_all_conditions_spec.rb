7128e550e381786be597e14cb4f937581b39e8e5
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-08 12:56:59 -0600

    adds labels to the information given, 'Date: ' etc.

--- spec/features/condition/visitor_sees_all_conditions_spec.rb
+++ spec/features/condition/visitor_sees_all_conditions_spec.rb
@@ -10,15 +10,16 @@ describe 'as a visitor' do
       visit conditions_path
 
       expect(page).to have_content(header)
+
       within('.condition-number-1') do
-        expect(page).to have_content(@condition.date)
-        expect(page).to have_content(@condition.max_temperature)
-        expect(page).to have_content(@condition.mean_temperature)
-        expect(page).to have_content(@condition.min_temperature)
-        expect(page).to have_content(@condition.mean_humidity)
-        expect(page).to have_content(@condition.mean_visibility)
-        expect(page).to have_content(@condition.mean_wind_speed)
-        expect(page).to have_content(@condition.precipitation)
+        expect(page).to have_content("Date: #{@condition.date}")
+        expect(page).to have_content("Max Temperature: #{@condition.max_temperature}")
+        expect(page).to have_content("Mean Temperature: #{@condition.mean_temperature}")
+        expect(page).to have_content("Min Temperature: #{@condition.min_temperature}")
+        expect(page).to have_content("Mean Humidity: #{@condition.mean_humidity}")
+        expect(page).to have_content("Mean Visibility: #{@condition.mean_visibility}")
+        expect(page).to have_content("Mean Wind Speed: #{@condition.mean_wind_speed}")
+        expect(page).to have_content("Precipitation: #{@condition.precipitation}")
       end
     end
   end
