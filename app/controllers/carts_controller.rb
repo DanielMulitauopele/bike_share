2c612a5a328cc3868351551255be4582ca6901e5
Patrick Shobe <patrick@patrickshobe.com>
2018-09-08 17:59:05 -0600

    Fixes nil class issue

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,8 @@
 class CartsController < ApplicationController
   def create
-    require "pry"; binding.pry
     accessory = Accessory.find(params[:accessory_id])
     session[:cart] ||= Hash.new(0)
-    # require "pry"; binding.pry
-    session[:cart][accessory.id] += 1
+    session[:cart][accessory.id.to_s] += 1
     flash[:notice] = "You have added an #{accessory.title} to your cart!"
     redirect_to bike_shop_path
   end
