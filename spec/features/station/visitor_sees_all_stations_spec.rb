93cf9c6a71b6876b30dd05be117a548503cd1d3e
Tara Craig <tara.craig7@gmail.com>
2018-09-08 11:26:51 -0600

    Add link to show page for each station on the stations index. ATP.

--- spec/features/station/visitor_sees_all_stations_spec.rb
+++ spec/features/station/visitor_sees_all_stations_spec.rb
@@ -9,11 +9,11 @@ describe 'visitor sees all stations' do
     it "displays all stations" do
       visit stations_path
 
-      expect(page).to have_content(@station1.name)
+      expect(page).to have_link(@station1.name)
       expect(page).to have_content(@station1.dock_count)
       expect(page).to have_content(@station1.city)
       expect(page).to have_content(@station1.installation_date)
-      expect(page).to have_content(@station2.name)
+      expect(page).to have_link(@station2.name)
       expect(page).to have_content(@station2.dock_count)
       expect(page).to have_content(@station2.city)
       expect(page).to have_content(@station2.installation_date)
