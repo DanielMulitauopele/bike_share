c39c0d4c3660f41966a83035823717ccac62a5a2
Tara Craig <tara.craig7@gmail.com>
2018-09-08 12:25:13 -0600

    Add test to verify current path is showing station slug instead of id.

--- spec/features/station/visitor_sees_one_station_spec.rb
+++ spec/features/station/visitor_sees_one_station_spec.rb
@@ -11,10 +11,11 @@ describe 'visitor sees one station' do
 
       click_on @station.name
 
+      expect(current_path).to eq('/stations/wash-park-east')
       expect(page).to have_content(@station.name)
       expect(page).to have_content(@station.dock_count)
       expect(page).to have_content(@station.city)
       expect(page).to have_content(@station.installation_date)
     end
   end
-end 
+end
