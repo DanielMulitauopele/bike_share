0e7782c6977b52bcae74f5be52ad138246468ab4
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-08 13:49:29 -0600

    Deletes save_and_open_page

--- spec/features/users/user_authentication_spec.rb
+++ spec/features/users/user_authentication_spec.rb
@@ -8,7 +8,6 @@ describe 'user can visit login page' do
       
       expect(current_path).to eq(login_path)
       expect(page).to have_content('Please log in or sign up if you are not a registered user, yet!')
-      save_and_open_page
     end 
   end
 end 
