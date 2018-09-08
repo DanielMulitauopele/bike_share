3b1ec29f5d021461b485e05f907e92666398abdf
Tara Craig <tara.craig7@gmail.com>
2018-09-08 14:00:40 -0600

    Add more to accessories index spec.

--- spec/features/accessory/visitor_sees_all_bike_accessories_spec.rb
+++ spec/features/accessory/visitor_sees_all_bike_accessories_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 describe 'visitor sees all bike accessories' do
   describe 'they visit bike shop' do
-    it "displays all bike accessories" do
+    before(:each) do
+      @accessory = create(:accessory)
+    end
 
+    it "displays all bike accessories" do
+      visit 
     end
   end
 end
