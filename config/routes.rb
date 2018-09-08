deb1b4b5e88601a162fb295a6bcc7b9277e58a2f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-08 13:40:40 -0600

    Adds login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  
   resources :stations
 end
---
+++ spec/features/users/user_authentication_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper' 
+
+describe 'user can visit login page' do 
+  describe 'they link from the root page' do
+    it 'should allow user to vist login page' do 
+      
+      visit root_path 
+      click_link 'Login'
+      
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content('Please log in or sign up if you are not a registered user, yet!')
+    end 
+  end
+end 
