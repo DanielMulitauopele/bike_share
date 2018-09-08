95c0b46d118ec82e3ae03398ec56ce4a8ea8de3a
Patrick Shobe <patrick@patrickshobe.com>
2018-09-08 14:32:27 -0600

    Closes #4

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  
+
   resources :stations
 
-  resources :trips, only: [:index]
+  resources :trips, only: [:index, :show]
 
   resources :conditions, only: [:index]
 
