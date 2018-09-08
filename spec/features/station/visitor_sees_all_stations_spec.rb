f43059e90189da28fc10b91970e59644b677425a
Tara Craig <tara.craig7@gmail.com>
2018-09-07 23:21:47 -0600

    Add friendly_id gem to generate slugs for stations. Refactor tests to include validations for uniqueness of slugs. Add validate uniqueness to station model. Add show method to station controller with text specified by friendly_id setup page. ATP.

--- spec/features/station/visitor_sees_all_stations_spec.rb
+++ spec/features/station/visitor_sees_all_stations_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'visitor sees all stations' do
   describe 'they visit stations index' do
     before(:each) do
-      @station1 = Station.create(name: 'Wash Park East', dock_count: 10, city: 'Denver', installation_date: '2018-09-01', slug: 'WashParkEast')
-      @station2 = Station.create(name: 'Yale St', dock_count: 5, city: 'Englewood', installation_date: '2018-09-02', slug: 'YaleSt')
+      @station1 = Station.create(name: 'Wash Park East', dock_count: 10, city: 'Denver', installation_date: '2018-09-01')
+      @station2 = Station.create(name: 'Yale St', dock_count: 5, city: 'Englewood', installation_date: '2018-09-02')
     end
     it "displays all stations" do
       visit stations_path
