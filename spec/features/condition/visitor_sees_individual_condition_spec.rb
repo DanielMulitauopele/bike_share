d17995ff82b6a9c55646f74aa9122fdc287389a8
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-08 14:22:07 -0600

    adds details to condition show page

--- spec/features/condition/visitor_sees_individual_condition_spec.rb
+++ spec/features/condition/visitor_sees_individual_condition_spec.rb
@@ -13,5 +13,17 @@ describe 'as a visitor' do
       expect(current_path).to eq(condition_path(@condition))
       expect(page).to have_content(header)
     end
+    it 'should show the details of a specific condition' do
+      header = "Conditions for #{@condition.date}"
+      visit condition_path(@condition)
+
+      expect(page).to have_content("Max Temperature: #{@condition.max_temperature}")
+      expect(page).to have_content("Mean Temperature: #{@condition.mean_temperature}")
+      expect(page).to have_content("Min Temperature: #{@condition.min_temperature}")
+      expect(page).to have_content("Mean Humidity: #{@condition.mean_humidity}")
+      expect(page).to have_content("Mean Visibility: #{@condition.mean_visibility}")
+      expect(page).to have_content("Mean Wind Speed: #{@condition.mean_wind_speed}")
+      expect(page).to have_content("Precipitation: #{@condition.precipitation}")
+    end
   end
 end
