af18b858a439026186906e4018ffc425ca74e516
Tara Craig <tara.craig7@gmail.com>
2018-09-08 20:52:09 -0600

    Add number to currency helper method to the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  helper_method :current_user
-  
+
+  helper_method :current_user, :number_to_currency
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def number_to_currency(number)
+    ('$%.2f' % number).to_s
   end
 end
