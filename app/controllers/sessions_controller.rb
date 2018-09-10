d726f4a6e926e8ec9241dd05eb1f8722498a8ed2
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-10 17:12:51 -0600

    Adds missing authentication tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:login][:email].downcase)
+    @user = User.find_by(email: params[:login][:email].downcase)
 
-    if user && user.authenticate(params[:login][:password])
-      session[:user_id] = user.id.to_s
-      redirect_to root_path, notice: 'Successfully logged in!'
+    if @user && @user.authenticate(params[:login][:password])
+      session[:user_id] = @user.id.to_s
+      redirect_to dashboard_path, notice: 'You have been successfully logged in!'
     else
-      flash.now.alert = "Incorrect email or password, try again."
+      flash.now.alert = 'Incorrect email or password, please try again.'
       render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to login_path, notice: "Logged out!"
+    redirect_to bike_shop_path, notice: 'You have been successfully logged out!'
   end
-
-
 end
