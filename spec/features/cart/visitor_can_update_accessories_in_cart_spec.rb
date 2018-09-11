afd39b8562f4e6a73f553266883ca04e6ecdb3c9
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-11 14:31:45 -0600

    adds subtraction test

--- spec/features/cart/visitor_can_update_accessories_in_cart_spec.rb
+++ spec/features/cart/visitor_can_update_accessories_in_cart_spec.rb
@@ -19,5 +19,23 @@ describe 'as a visitor' do
       click_on '+'
       expect(page).to have_content(2)
     end
+    it "should be able to increase quantity" do
+      accessory = create(:accessory)
+      visit bike_shop_path
+
+      click_button 'Add to Cart'
+      click_button 'Add to Cart'
+      expect(page).to have_content("You now have 2 of  #{accessory.title} in your cart.")
+
+      within '#Cart-Header' do
+        click_on 'Cart'
+      end
+
+      expect(page).to have_content(accessory.title)
+      expect(page).to have_content(2)
+
+      click_on '1'
+      expect(page).to have_content(1)
+    end
   end
 end
