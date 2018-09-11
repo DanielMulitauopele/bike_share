af22e823c6cf50bab474eb8d660ccd9d2d8d379d
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-11 15:27:48 -0600

    deletes uncommented test

--- spec/features/cart/user_sees_cart_spec.rb
+++ spec/features/cart/user_sees_cart_spec.rb
@@ -41,28 +41,3 @@ describe 'user sees all items in cart' do
     expect(page).to have_content(90)
   end
 end
-
-# describe 'When user clicks on checkout they are directed to their dashboard page' do 
-#   describe 'they link from the cart page' do 
-#     it 'should direct user to their dashboard' do 
-#       accessory_1 = create(:accessory)
-#       accessory_2 = create(:accessory, title: 'an accessory')
-#       user = create(:user)
-# 
-#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-# 
-#       visit carts_path
-# 
-#       expect(page).to have_content(accessory_1.title)
-#       expect(page).to have_content("price: #{accessory_1.price}")
-#       expect(page).to have_content(accessory_2.title)
-#       expect(page).to have_content("price: #{accessory_2.price}")
-#       expect(page).to have_content(90)
-# 
-#       click_on 'checkout'
-# 
-#       expect(current_path).to eq(dashboard_path)
-#       expect(page).to have_content('You have have successfully submitted your order totaling $total')
-#     end 
-#   end 
-# end
