f0913233ba2d20f71834cb2764865aa4254c5671
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-11 18:58:17 -0600

    Adds first checkout logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,16 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+  
+  def create
+    if current_user
+      cureent_user.orders.create
+      redirect_to dashboard_path
+      flash[:notice] = 'You have successfully submitted your order totaling $total'
+    else 
+      redirect_to login_path
+      flash[:alert] = 'Please login first'
+    end  
+  end
+
 end
--- spec/features/cart/user_sees_cart_spec.rb
+++ spec/features/cart/user_sees_cart_spec.rb
@@ -41,3 +41,40 @@ describe 'user sees all items in cart' do
     expect(page).to have_content(90)
   end
 end
+
+describe 'When user clicks on checkout after adding items to cart, they are directed to their dashboard page' do 
+  describe 'they link from the cart page' do 
+    it 'should direct user to their dashboard' do 
+      accessory_1 = create(:accessory)
+      accessory_2 = create(:accessory, title: 'an accessory')
+      user = create(:user, role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit bike_shop_path
+
+      expect(page).to have_content("Cart: 0")
+
+      within("#accessory-#{accessory_1.id}") do
+        click_on 'Add to Cart'
+      end
+      within("#accessory-#{accessory_2.id}") do
+        click_on 'Add to Cart'
+      end
+      within('#Cart-Header') do
+        click_on 'Cart'
+      end
+      expect(current_path).to eq(carts_path)
+      expect(page).to have_content(accessory_1.title)
+      expect(page).to have_content("price: #{accessory_1.price}")
+      expect(page).to have_content(accessory_2.title)
+      expect(page).to have_content("price: #{accessory_2.price}")
+      expect(page).to have_content(60)
+
+      click_on 'Checkout'
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content('You have successfully submitted your order totaling $total')
+    end 
+  end 
+end
