9a18ee8dfe59898ace1fcd6a1b455b78d30d75a6
Patrick Shobe <patrick@patrickshobe.com>
2018-09-13 19:38:19 -0600

    Adds edit button and edit functionality

--- app/controllers/admin/conditions_controller.rb
+++ app/controllers/admin/conditions_controller.rb
@@ -5,4 +5,22 @@ class Admin::ConditionsController < Admin::BaseController
     flash[:notice] = "Successfully deleted #{condition.date}"
     redirect_to conditions_path
   end
+
+  def edit
+    @condition = Condition.find(params[:id])
+  end
+
+  def update
+    @condition = Condition.find(params[:id])
+    @condition.update(condition_params)
+    redirect_to condition_path(@condition)
+  end
+
+  private
+  def condition_params
+    params.require(:condition).permit(:date, :max_temperature, :mean_temperature,
+                                      :min_temperature, :mean_humidity,
+                                      :mean_visibility, :mean_wind_speed,
+                                      :precipitation)
+  end
 end
