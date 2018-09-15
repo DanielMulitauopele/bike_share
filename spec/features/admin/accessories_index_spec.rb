a859c83db7f8de243cbe40aeb7be7e112f4131e6
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-15 16:09:27 -0600

    Adds admin trips index functionality

--- spec/features/admin/accessories_index_spec.rb
+++ spec/features/admin/accessories_index_spec.rb
@@ -8,21 +8,21 @@ describe 'Visit the Admin Bike Shop Path' do
       @accessory3 = create(:accessory, title: 'Thing3')
       @accessory4 = create(:accessory, title: 'Thing4')
     end
+    
     it 'should link from admin_dashboard' do
-      admin = create(:user, role: 1)
+    admin = create(:user, role: 1)
 
-     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-     visit admin_dashboard_path
+    visit admin_dashboard_path
 
-     click_link 'All Accessories'
+    click_link 'All Accessories'
 
-     expect(current_path).to eq(admin_bike_shop_path)
-      expect(page).to have_content(@accessory1.title)
-      expect(page).to have_content(@accessory2.title)
-      expect(page).to have_content(@accessory3.title)
-      expect(page).to have_content(@accessory4.title)
-
+    expect(current_path).to eq(admin_bike_shop_path)
+    expect(page).to have_content(@accessory1.title)
+    expect(page).to have_content(@accessory2.title)
+    expect(page).to have_content(@accessory3.title)
+    expect(page).to have_content(@accessory4.title)
     end
   end
 end
