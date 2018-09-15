482239a56be0f46cf003f0df5015552ca112e7cb
Tara Craig <tara.craig7@gmail.com>
2018-09-15 15:39:28 -0600

    Add ability for admin to create new trips. ATP.

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,4 +1,22 @@
 class Admin::TripsController < Admin::BaseController
 
+  def new
+    @trip = Trip.new
+  end
 
-end 
+  def create
+    @trip = Trip.create(trip_params)
+    if @trip.save
+      flash[:notice] = "Trip successfully created!"
+      redirect_to trip_path(@trip)
+    else
+      flash[:alert] = 'Oops, something went wrong, please try again!'
+      render :new
+    end
+  end
+
+  private
+    def trip_params
+      params.require(:trip).permit(:duration, :start_date, :start_station_id, :end_date, :end_station_id, :bike_id, :subscription_type, :zip_code)
+    end
+end
